347952d0adb116241ff84e48ae0bf6affed9a925
Faeze-Badiei <148384479+Faeze-Badiei@users.noreply.github.com>
2023-10-26 22:05:35 +0330

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -9,42 +9,45 @@ while True:
     
     n=int(input("Now enter a number:"))
 
-if s=="sin":
-    r=(math.sin(math.radians(n)))
+    if s=="sin":
+        r=math.sin(math.radians(n))
 
-if s=="cos":
-    r=(math.cos(math.radians(n)))
+    if s=="cos":
+        r=math.cos(math.radians(n))
 
-if s=="tan":
-    r=(math.tan(math.radians(n)))
+    if s=="tan":
+        r=math.tan(math.radians(n))
 
-if s=="cot":
-    z=(math.tan(math.radians(n)))
-    r=1/z
+    if s=="cot":
+        z=math.tan(math.radians(n))
+        r=1/z
 
-if s=="sqrt":
-    r=(math.sqrt(n))
+    if s=="sqrt":
+        if n<0:
+            "Error!"
+        else:
+            r=math.sqrt(n)
 
-if s=="factorial":
-    r=(math.factorial(n))
+    if s=="factorial":
+        r=(math.factorial(n))
 
-if s=="+":
-    a=float(input("please enter another number:")) 
-    r=n+a
+    if s=="+":
+        a=float(input("please enter another number:")) 
+        r=n+a
 
-if s == "-":
-    b=float(input("please enter another number:"))
-    r=n-b
+    if s == "-":
+        b=float(input("please enter another number:"))
+        r=n-b
 
-if s=="*":
-    c=float(input("please enter another number:")) 
-    r=n*c
+    if s=="*":
+        c=float(input("please enter another number:")) 
+        r=n*c
 
-if s == "/": 
-    d=float(input("please enter another number:"))
-    if d==0:
-        r="Error! plaese try with another number"
-    else:
-        r=n/d
+    if s == "/": 
+        d=float(input("please enter another number:"))
+        if d==0:
+            r="Error! plaese try with another number"
+        else:
+            r=n/d
 
-print(r)
+    print(r)
