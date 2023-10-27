6390915546d7b9d0849aaa2890a7a7ed7908471a
Faeze-Badiei <148384479+Faeze-Badiei@users.noreply.github.com>
2023-10-27 20:50:15 +0330

    4.Dice.py

--- 4.Dice.py
+++ 4.Dice.py
@@ -1,10 +1,13 @@
 import random
 
 while True:
-    d=random.randint(1,6)
-    print(d)
-    if d==6:
+    R = input("roll or exit:")
+    if R == "exit":
+        break
+    if R == "roll":
         d=random.randint(1,6)
         print(d)
+        if d==6:
+            d=random.randint(1,6)
+            print(d)
     break
-    
