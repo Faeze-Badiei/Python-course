dfdb3a8ca887b22f9ac5b818b2b0605f9b1ca535
Faeze-Badiei <148384479+Faeze-Badiei@users.noreply.github.com>
2023-11-04 23:18:34 +0330

    5.checkered.py

--- 5.checkered.py
+++ 5.checkered.py
@@ -3,5 +3,8 @@ B = int(input("Enter a number for aother side: "))
 
 for i in range(F):
     for j in range(B):
-        print("|""_", end="")
+        if(i+j)%2==0:
+            print("|", end="")
+        else:
+            print("_", end="")
     print()
